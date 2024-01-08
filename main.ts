b57e12fb1889e3593563b88d1a1d4dcd51d96ac6
Anna3688 <155963792+Anna3688@users.noreply.github.com>
2024-01-08 10:18:23 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+for (let index = 0; index < 3; index++) {
+    basic.showString("loading...")
+}
+basic.forever(function () {
+    basic.showString("Hello my name is Anna!")
+    basic.pause(2000)
+})
